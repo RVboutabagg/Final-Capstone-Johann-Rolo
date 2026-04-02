830b5afd2a345cb3ee558d31f306349f7c645e18
RVboutabagg <rolandovega2007@gmail.com>
2026-04-01 21:37:35 -0700

    isjfiwanjapkwijoao

---
+++ Components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+      <footer className="bg-gray-900 text-gray-400 text-center text-sm py-4 mt-auto">
+        © Pothole Tracker Inc. All Rights Reserved.
+      </footer>
+    );
+  }
+  
+  export default Footer;
